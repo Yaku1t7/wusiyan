1578458726257d560354682c2b7a6f31447d7632
wusiyan <155792208+Yaku1t7@users.noreply.github.com>
2024-01-06 11:33:50 +0800

    Add files via upload

---
+++ game.c
@@ -0,0 +1,144 @@
+﻿#define  _CRT_SECURE_NO_WARNINGS 1
+#pragma warning(disable:6031)
+#include"game.h"
+void InitBoard(char board[ROW][COL], int row, int col)
+{
+	int i, j;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			board[i][j] = ' ';
+		}
+	}
+}
+//void  DisplayBoard(char board[ROW][COL], int row, int col)
+//{
+//	int i;
+//	for (i = 0; i < row; i++)
+//	{
+//		printf(" %c | %c | %c \n", board[i][0], board[i][1], board[i][2]);
+//		if (i < row - 1)
+//		{
+//			printf("---|---|---\n");//only when 3x3,but if 10x10?
+//		}
+//	}
+//}
+void  DisplayBoard(char board[ROW][COL], int row, int col)
+{
+	int i, j;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			printf(" %c ", board[i][j]);
+			if (j < col - 1)
+			{
+				printf("|");
+			}
+		}
+		printf("\n");
+		if (i < row - 1)
+		{
+			for (j = 0; j < col; j++)
+			{
+				printf("---");
+				if (j < col - 1)
+				{
+					printf("|");
+				}
+			}
+			printf("\n");
+		}
+	}
+}
+void PlayerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("Your turn:>\n");
+	while (1)
+	{
+		printf("Please enter your coordinate:>\n");
+		scanf("%d%d", &x, &y);
+		//Judging the legality of changing coordinates x and y
+		if (x >= 1 && x <= row && y >= 1 && y <= col)
+		{
+			if (board[x - 1][y - 1] == ' ')
+			{
+				board[x - 1][y - 1] = '*';
+				break;
+			}
+			else
+			{
+				printf("This coordinate is occupied!\n");
+			}
+		}
+		else
+		{
+			printf("This coordinate is illegal，Please re-enter!\n");
+		}
+	}
+}
+void ComputerMove(char board[ROW][COL], int row, int col)
+{
+	int x = 0;
+	int y = 0;
+	printf("Computer's turn:>\n");
+	while (1)
+	{
+		x = rand() % ROW;
+		y = rand() % COL;//let x and y in [0,2] randomly
+		if (board[x][y] == ' ')
+		{
+			board[x][y] = '#';
+			break;
+		}
+	}
+}
+int Isdraw(char board[ROW][COL], int row, int col)
+{
+	int i, j;
+	for (i = 0; i < row; i++)
+	{
+		for (j = 0; j < col; j++)
+		{
+			if (board[i][j] == ' ')
+			{
+				return 0;//The chessboard is not full
+			}
+		}
+	}
+	return 1;
+}
+char Iswin(char board[ROW][COL], int row, int col)
+{
+	int i;
+	for (i = 0; i < row; i++)
+	{
+		if (board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != ' ')
+		{
+			return board[i][0];//if return * -->player win;if return # -->computer win
+		}
+	}
+	for (i = 0; i < col; i++)
+	{
+		if (board[0][i] == board[1][i] && board[1][i] == board[2][i] && board[0][i] != ' ')
+		{
+			return board[0][i];//if return * -->player win;if return # -->computer win
+		}
+	}
+	if (board[0][0] == board[1][1] && board[1][1] == board[2][2] && board[1][1] != ' ')
+	{
+		return board[0][0];//if return * -->player win;if return # -->computer win
+	}
+	if (board[0][2] == board[1][1] && board[1][1] == board[2][0] && board[1][1] != ' ')
+	{
+		return board[0][2];//if return * -->player win;if return # -->computer win
+	}
+	if (1 == Isdraw(board, ROW, COL))
+	{
+		return 'D';
+	}
+	return 'C';
+}
